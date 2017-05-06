99f8b68f43c1bf282225a9fcd5a1ebe0e3989074
Michael Henderson <services@michaelrhenderson.com>
2017-05-06 23:05:37 +0100

    Added background change on MainActivity

--- app/src/main/java/com/example/jamesb/dopeplayer/MainActivity.java
+++ app/src/main/java/com/example/jamesb/dopeplayer/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.jamesb.dopeplayer;
 
 
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
 public class MainActivity extends BaseActivity{
 
+    RelativeLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        layout = (RelativeLayout) findViewById(R.id.background);
+        layout.setBackgroundResource(R.drawable.background_gradient);
+
+    }
 
     @Override
     int getContentViewId() {
